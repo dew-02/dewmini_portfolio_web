804665532694e114d15914a2b02e8fd5791f008c
Dewmini Jayasinghe <dewminichalakshana456@gmail.com>
2026-03-04 11:33:35 +0530

    route changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 // src/components/Navbar.jsx
 import React from "react";
-import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link"; // smooth hash linking
 
 export default function Navbar() {
   return (
@@ -8,21 +8,11 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
         <div className="text-2xl font-bold text-white">Dewmini Chalakshana</div>
         <ul className="flex space-x-6 text-white">
-          <li>
-            <Link to="/" className="hover:text-blue-400">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className="hover:text-blue-400">About</Link>
-          </li>
-          <li>
-            <Link to="/skills" className="hover:text-blue-400">Skills</Link>
-          </li>
-          <li>
-            <Link to="/projects" className="hover:text-blue-400">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact" className="hover:text-blue-400">Contact</Link>
-          </li>
+          <li><Link smooth to="#/" className="hover:text-blue-400">Home</Link></li>
+          <li><Link smooth to="/about" className="hover:text-blue-400">About</Link></li>
+          <li><Link smooth to="/skills" className="hover:text-blue-400">Skills</Link></li>
+          <li><Link smooth to="/projects" className="hover:text-blue-400">Projects</Link></li>
+          <li><Link smooth to="/contact" className="hover:text-blue-400">Contact</Link></li>
         </ul>
       </div>
     </nav>
