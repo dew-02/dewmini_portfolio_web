36364de35e1645851edd1de76df3a2604b11ec0b
Dewmini Jayasinghe <dewminichalakshana456@gmail.com>
2026-03-04 11:42:31 +0530

    agian change files route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
