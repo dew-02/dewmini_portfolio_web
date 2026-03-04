b67e29c3c61742f31bca9351fe781c4e3e463ad2
Dewmini Jayasinghe <dewminichalakshana456@gmail.com>
2026-03-04 11:25:01 +0530

    update navbar page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 // src/components/Navbar.jsx
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -7,12 +8,22 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
         <div className="text-2xl font-bold text-white">Dewmini Chalakshana</div>
         <ul className="flex space-x-6 text-white">
-  <li><a href="/" className="hover:text-blue-400">Home</a></li>
-<li><a href="/about" className="hover:text-blue-400">About</a></li>
-  <li><a href="/skills" className="hover:text-blue-400">Skills</a></li>
-  <li><a href="/projects" className="hover:text-blue-400">Projects</a></li>
-  <li><a href="/contact" className="hover:text-blue-400">Contact</a></li>
-</ul>
+          <li>
+            <Link to="/" className="hover:text-blue-400">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-blue-400">About</Link>
+          </li>
+          <li>
+            <Link to="/skills" className="hover:text-blue-400">Skills</Link>
+          </li>
+          <li>
+            <Link to="/projects" className="hover:text-blue-400">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-blue-400">Contact</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
